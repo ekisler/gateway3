fbfce9b6dfa5615164ca078ac4f0bdd5266c52ed
Emisael Kisler <ekisler@gmail.com>
2023-05-18 19:42:07 -0400

    First commit

---
+++ gateway/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const PORT = 8000;
+
+const app = express();
+
+app.use(
+  "/characters",
+  createProxyMiddleware({
+    target: "http://characters:8001",
+    changeOrigin: true,
+  })
+);
+
+app.use(
+  "/films",
+  createProxyMiddleware({
+    target: "http://films:8002",
+    changeOrigin: true,
+  })
+);
+
+app.use(
+  "/planets",
+  createProxyMiddleware({
+    target: "http://planets:8003",
+    changeOrigin: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Gateway conectado correctamente en el PUERTO: ${PORT} 💥`);
+});
